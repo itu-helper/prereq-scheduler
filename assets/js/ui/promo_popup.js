0b3211899349cd32e8a3e0eec732bbb75f956b68
Ata Türkoğlu <ataturkoglu2003@hotmail.com>
2025-10-17 13:42:28 +0300

    Chrome Extension Promo
    - Added promo popup system
    - Added chrome extension popup

---
+++ assets/js/ui/promo_popup.js
@@ -0,0 +1,23 @@
+const promotion_identifier = "chrome_extension_release";
+
+document.addEventListener('DOMContentLoaded', function() {
+    id = "promo_" + promotion_identifier
+    const popup = document.getElementById('extensionPopup');
+    const closeButton = document.getElementById('closePopup');
+    const hideCheckbox = document.getElementById('hideExtensionPopup');
+
+    const isPopupHidden = localStorage.getItem(id);
+
+    if (!isPopupHidden) {
+        popup.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+    }
+
+    closeButton.addEventListener('click', function () {
+        if (hideCheckbox.checked) {
+            localStorage.setItem(id, 'true');
+        }
+        popup.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    });
+});
